4599e4150a62f372a6c463151c09e00a80c7bb43
Eray Ozkural <examachine@gmail.com>
2017-02-19 02:25:36 +0300

    add dbg arg
    * —debug is passed to all pisi invocations

--- tests/light.sh
+++ tests/light.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 # Author:  Eray Ozkural <eray@pardus.org.tr>
-
+dbg=--debug
 pwd
 PATH=$PATH:.
 set -x
 rm -rf tmp
 mkdir tmp
 set -e
-pisi-cli -Dtmp -E --ignore-build-no build tests/zip/pspec.xml tests/unzip/pspec.xml
-pisi-cli -Dtmp index .
-pisi-cli -Dtmp -d add-repo repo1 pisi-index.xml
-pisi-cli -Dtmp list-repo
-pisi-cli -Dtmp update-repo repo1
-pisi-cli -Dtmp list-available
-pisi-cli -Dtmp --ignore-comar -y install zip
-pisi-cli -Dtmp list-installed
-pisi-cli -Dtmp --ignore-comar -y remove unzip
-pisi-cli -Dtmp info zip*.pisi
-pisi-cli -d -Dtmp --ignore-comar -y install zip*.pisi
-pisi-cli -Dtmp list-pending
-pisi-cli -Dtmp configure-pending
-pisi-cli -Dtmp remove-repo repo1
-pisi-cli -Dtmp clean
+pisi-cli -Dtmp $dbg -E --ignore-build-no build tests/zip/pspec.xml tests/unzip/pspec.xml
+pisi-cli -Dtmp $dbg index .
+pisi-cli -Dtmp $dbg -d add-repo repo1 pisi-index.xml
+pisi-cli -Dtmp $dbg list-repo
+pisi-cli -Dtmp $dbg update-repo repo1
+pisi-cli -Dtmp $dbg list-available
+pisi-cli -Dtmp $dbg --ignore-comar -y install zip
+pisi-cli -Dtmp $dbg list-installed
+pisi-cli -Dtmp $dbg --ignore-comar -y remove unzip
+pisi-cli -Dtmp $dbg info zip*.pisi
+pisi-cli -d -Dtmp $dbg --ignore-comar -y install zip*.pisi
+pisi-cli -Dtmp $dbg list-pending
+pisi-cli -Dtmp $dbg configure-pending
+pisi-cli -Dtmp $dbg remove-repo repo1
+pisi-cli -Dtmp $dbg clean
