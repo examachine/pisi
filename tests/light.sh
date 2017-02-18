e1e05d04d93966416c906a883be98f9f37a63410
Eray Ozkural <examachine@gmail.com>
2017-02-19 02:34:14 +0300

    do not debug

--- tests/light.sh
+++ tests/light.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Author:  Eray Ozkural <eray@pardus.org.tr>
-dbg=--debug
+dbg=
+#dbg=-d
 pwd
 PATH=$PATH:.
 set -x
@@ -9,7 +10,7 @@ mkdir tmp
 set -e
 pisi-cli -Dtmp $dbg -E --ignore-build-no build tests/zip/pspec.xml tests/unzip/pspec.xml
 pisi-cli -Dtmp $dbg index .
-pisi-cli -Dtmp $dbg -d add-repo repo1 pisi-index.xml
+pisi-cli -Dtmp $dbg add-repo repo1 pisi-index.xml
 pisi-cli -Dtmp $dbg list-repo
 pisi-cli -Dtmp $dbg update-repo repo1
 pisi-cli -Dtmp $dbg list-available
@@ -17,7 +18,7 @@ pisi-cli -Dtmp $dbg --ignore-comar -y install zip
 pisi-cli -Dtmp $dbg list-installed
 pisi-cli -Dtmp $dbg --ignore-comar -y remove unzip
 pisi-cli -Dtmp $dbg info zip*.pisi
-pisi-cli -d -Dtmp $dbg --ignore-comar -y install zip*.pisi
+pisi-cli -Dtmp $dbg --ignore-comar -y install zip*.pisi
 pisi-cli -Dtmp $dbg list-pending
 pisi-cli -Dtmp $dbg configure-pending
 pisi-cli -Dtmp $dbg remove-repo repo1
